aad60abb5c69dc95cf26ac29d38120eb5834fec4
Kerolos Emad <90790284+kemad97@users.noreply.github.com>
2025-08-18 14:18:48 +0300

    add models

---
+++ lib/models/story.dart
@@ -0,0 +1,8 @@
+
+class Story {
+  final String name;
+  final String time;
+  final List<String> media;
+
+  Story({required this.name, required this.time, this.media = const ['images/story1.jpg', 'images/story2.jpg']});
+}
